766ad9050630e847405388ad68d139ba66d3b4f9
236621 Wiktor Pawlak <236621@edu.p.lodz.pl>
2023-03-11 21:26:56 +0100

    SSBD202306-7 Pierwsza wersja projektu

---
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd06/HelloApplication.java
@@ -0,0 +1,9 @@
+package pl.lodz.p.it.ssbd2023.ssbd06;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
