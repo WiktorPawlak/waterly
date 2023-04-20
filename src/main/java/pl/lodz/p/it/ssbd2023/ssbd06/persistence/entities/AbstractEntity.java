f189adecf9e3fa2ea46211eca383b304a0302deb
236621 Wiktor Pawlak <236621@edu.p.lodz.pl>
2023-04-21 00:03:49 +0200

    SSBD202306-82 Usunięcie danych audytowych na potrzeby spójności ze sprawozdaniem wstępnym

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd06/persistence/entities/AbstractEntity.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd06/persistence/entities/AbstractEntity.java
@@ -1,22 +1,15 @@
 package pl.lodz.p.it.ssbd2023.ssbd06.persistence.entities;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Version;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @ToString
@@ -32,24 +25,4 @@ public abstract class AbstractEntity implements Serializable {
 
     @Version
     private long version;
-
-    @CreationTimestamp
-    @Column(name = "created_on", updatable = false, nullable = false)
-    private LocalDateTime createdOn;
-
-    @ToString.Exclude
-    @OneToOne
-    @JoinColumn(name = "created_by", updatable = false)
-    @Setter
-    private Account createdBy;
-
-    @UpdateTimestamp
-    @Column(name = "updated_on")
-    @Setter
-    private LocalDateTime updatedOn;
-
-    @ToString.Exclude
-    @OneToOne
-    @JoinColumn(name = "updated_by")
-    private Account updatedBy;
 }
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -43,25 +43,30 @@ GRANT USAGE, SELECT ON SEQUENCE water_meter_check_id_seq TO ssbd06mol;
 GRANT USAGE, SELECT ON SEQUENCE water_meter_id_seq TO ssbd06mol;
 GRANT USAGE, SELECT ON SEQUENCE water_usage_stats_id_seq TO ssbd06mol;
 
--- add first administrator password admin12345
-INSERT INTO public.account_details (id, version, email, first_name, last_name, phone_number, created_on, updated_on) VALUES (nextval('account_details_id_seq'), 0, 'kontomatino@gmail.com', 'Mateusz', 'Strzelecki', '123456789', now(), now());
-INSERT INTO public.account (id, version, active, login, password, account_state, account_details_id, created_on, updated_on) VALUES (nextval('account_id_seq'), 0, true, 'admin', '$2a$04$m9vbbL2RTbV/XNC44TEZ0e.t9WH2Q6hjtyEem/siFCdNS564hW68q', 'CONFIRMED', 1, now(), now());
-INSERT INTO public.role (permission_level, id, active, version, account_id, created_on, updated_on) VALUES ('ADMINISTRATOR', nextval('role_id_seq'), true, 0, 1, now(), now());
-INSERT INTO public.role (permission_level, id, active, version, account_id, created_on, updated_on) VALUES ('FACILITY_MANAGER', nextval('role_id_seq'), true, 0, 1, now(), now());
-INSERT INTO public.role (permission_level, id, active, version, account_id, created_on, updated_on) VALUES ('OWNER', nextval('role_id_seq'), true, 0, 1, now(), now());
+-- Stworzenie konta administratora - hasło admin12345
+INSERT INTO public.account_details (id, version, email, first_name, last_name, phone_number) VALUES (nextval('account_details_id_seq'), 0, 'kontomatino@gmail.com', 'Mateusz', 'Strzelecki', '123456789');
+INSERT INTO public.account (id, version, active, login, password, account_state, account_details_id) VALUES (nextval('account_id_seq'), 0, true, 'admin', '$2a$04$m9vbbL2RTbV/XNC44TEZ0e.t9WH2Q6hjtyEem/siFCdNS564hW68q', 'CONFIRMED', 1);
+INSERT INTO public.role (permission_level, id, active, version, account_id) VALUES ('ADMINISTRATOR', nextval('role_id_seq'), true, 0, 1);
+INSERT INTO public.role (permission_level, id, active, version, account_id) VALUES ('FACILITY_MANAGER', nextval('role_id_seq'), false, 0, 1);
+INSERT INTO public.role (permission_level, id, active, version, account_id) VALUES ('OWNER', nextval('role_id_seq'), false, 0, 1);
 INSERT INTO public.administrator (id) VALUES (1);
-INSERT INTO public.auth_info (id, last_ip_address, last_success_auth, last_incorrect_auth, incorrect_auth_count, created_on, updated_on, version, account_id) VALUES (nextval('auth_info_id_seq'), null, null, null, 0, now(), now(), 0, 1);
--- add test OWNER password jantes123
-INSERT INTO public.account_details (id, version, email, first_name, last_name, phone_number, created_on, updated_on) VALUES (nextval('account_details_id_seq'), 0, 'kontosz@gmail.com', 'Szymon', 'Ziemecki', '123412341', now(), now());
-INSERT INTO public.account (id, version, active, login, password, account_state, account_details_id, created_on, updated_on) VALUES (nextval('account_id_seq'), 0, true, 'new', '$2a$04$j/yqCtlHxKmdxHMWxaji4OD1w591LIMNDGBqUbCpD6HTM4aj2uLiS', 'CONFIRMED', 2, now(), now());
-INSERT INTO public.role (permission_level, id, active, version, account_id, created_on, updated_on) VALUES ('OWNER', nextval('role_id_seq'), true, 0, 2, now(), now());
-INSERT INTO public.role (permission_level, id, active, version, account_id, created_on, updated_on) VALUES ('FACILITY_MANAGER', nextval('role_id_seq'), false, 0, 2, now(), now());
-INSERT INTO public.role (permission_level, id, active, version, account_id, created_on, updated_on) VALUES ('ADMINISTRATOR', nextval('role_id_seq'), false, 0, 2, now(), now());
+INSERT INTO public.owner (id) VALUES (1);
+INSERT INTO public.facility_manager (id) VALUES (1);
+-- Stworzenie konta zarządcy - hasło jantes123
+INSERT INTO public.account_details (id, version, email, first_name, last_name, phone_number) VALUES (nextval('account_details_id_seq'), 0, 'kontosz@gmail.com', 'Szymon', 'Ziemecki', '123412341');
+INSERT INTO public.account (id, version, active, login, password, account_state, account_details_id) VALUES (nextval('account_id_seq'), 0, true, 'new', '$2a$04$j/yqCtlHxKmdxHMWxaji4OD1w591LIMNDGBqUbCpD6HTM4aj2uLiS', 'CONFIRMED', 2);
+INSERT INTO public.role (permission_level, id, active, version, account_id) VALUES ('OWNER', nextval('role_id_seq'), false, 0, 2);
+INSERT INTO public.role (permission_level, id, active, version, account_id) VALUES ('FACILITY_MANAGER', nextval('role_id_seq'), true, 0, 2);
+INSERT INTO public.role (permission_level, id, active, version, account_id) VALUES ('ADMINISTRATOR', nextval('role_id_seq'), false, 0, 2);
+INSERT INTO public.administrator (id) VALUES (2);
 INSERT INTO public.owner (id) VALUES (2);
-INSERT INTO public.auth_info (id, last_ip_address, last_success_auth, last_incorrect_auth, incorrect_auth_count, created_on, updated_on, version, account_id) VALUES (nextval('auth_info_id_seq'), null, null, null, 0, now(), now(), 0, 2);
--- add test FACILITY MANGER password jantes123
-INSERT INTO public.account_details (id, version, email, first_name, last_name, phone_number, created_on, updated_on) VALUES (nextval('account_details_id_seq'), 0, 'tomdut@gmail.com', 'Tom', 'Dut', '666666666', now(), now());
-INSERT INTO public.account (id, version, active, login, password, account_state, account_details_id, created_on, updated_on) VALUES (nextval('account_id_seq'), 0, true, 'tomdut', '$2a$04$j/yqCtlHxKmdxHMWxaji4OD1w591LIMNDGBqUbCpD6HTM4aj2uLiS', 'CONFIRMED', 3, now(), now());
-INSERT INTO public.role (permission_level, id, active, version, account_id, created_on, updated_on) VALUES ('OWNER', nextval('role_id_seq'), true, 0, 3, now(), now());
+INSERT INTO public.facility_manager (id) VALUES (2);
+-- Stworzenie konta właściciela lokalu - hasło jantes123
+INSERT INTO public.account_details (id, version, email, first_name, last_name, phone_number) VALUES (nextval('account_details_id_seq'), 0, 'tomdut@gmail.com', 'Tom', 'Dut', '666666666');
+INSERT INTO public.account (id, version, active, login, password, account_state, account_details_id) VALUES (nextval('account_id_seq'), 0, true, 'tomdut', '$2a$04$j/yqCtlHxKmdxHMWxaji4OD1w591LIMNDGBqUbCpD6HTM4aj2uLiS', 'CONFIRMED', 3);
+INSERT INTO public.role (permission_level, id, active, version, account_id) VALUES ('OWNER', nextval('role_id_seq'), true, 0, 3);
+INSERT INTO public.role (permission_level, id, active, version, account_id) VALUES ('FACILITY_MANAGER', nextval('role_id_seq'), false, 0, 3);
+INSERT INTO public.role (permission_level, id, active, version, account_id) VALUES ('ADMINISTRATOR', nextval('role_id_seq'), false, 0, 3);
+INSERT INTO public.administrator (id) VALUES (3);
+INSERT INTO public.owner (id) VALUES (3);
 INSERT INTO public.facility_manager (id) VALUES (3);
-INSERT INTO public.auth_info (id, last_ip_address, last_success_auth, last_incorrect_auth, incorrect_auth_count, created_on, updated_on, version, account_id) VALUES (nextval('auth_info_id_seq'), null, null, null, 0, now(), now(), 0, 3);
