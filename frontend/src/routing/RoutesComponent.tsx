4e1ed5b3ed0d29391e32e997f39f4c03cafe297d
236680 Weronika Tutkaj <nikat2001@gmail.com>
2023-05-24 20:05:48 +0200

    SSBD202306-281 wybor poziomu dostepu

--- frontend/src/routing/RoutesComponent.tsx
+++ frontend/src/routing/RoutesComponent.tsx
@@ -1,42 +1,48 @@
-import {roles} from "../types";
-import {Loading} from "../layouts/components/Loading";
-import {adminRoutes, facilityManagerRoutes, ownerRoutes, Pathnames, publicRoutes,} from "./routes";
-import {useAccount} from "../hooks/useAccount";
-import {Navigate, Route, Routes} from "react-router-dom";
+import { roles } from "../types";
+import { Loading } from "../layouts/components/Loading";
+import {
+  adminRoutes,
+  facilityManagerRoutes,
+  ownerRoutes,
+  Pathnames,
+  publicRoutes,
+} from "./routes";
+import { useAccount } from "../hooks/useAccount";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 export const RoutesComponent = () => {
-    const {account, isLoading, hasRole} = useAccount();
+  const { account, isLoading } = useAccount();
 
-    if (isLoading) {
-        return <Loading/>;
-    }
+  if (isLoading) {
+    return <Loading />;
+  }
 
-    return (
-        <Routes>
-            {!account &&
-                publicRoutes.map(({path, Component}) => (
-                    <Route key={path} path={path} element={<Component/>}/>
-                ))}
+  return (
+    <Routes>
+      {!account &&
+        publicRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
 
-            {hasRole([roles.owner]) &&
-                ownerRoutes.map(({path, Component}) => (
-                    <Route key={path} path={path} element={<Component/>}/>
-                ))}
+      {account?.currentRole === roles.owner &&
+        ownerRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
 
-            {hasRole([roles.facilityManager]) &&
-                facilityManagerRoutes.map(({path, Component}) => (
-                    <Route key={path} path={path} element={<Component/>}/>
-                ))}
+      {account?.currentRole === roles.facilityManager &&
+        facilityManagerRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
 
-            {hasRole([roles.administrator]) &&
-                adminRoutes.map(({path, Component}) => (
-                    <Route key={path} path={path} element={<Component/>}/>
-                ))}
+      {account?.currentRole === roles.administrator &&
+        adminRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
 
-            <Route
-                path="*"
-                element={<Navigate to={Pathnames.public.notFound} replace/>}
-            />
-        </Routes>
-    );
+      <Route
+        path="*"
+        element={<Navigate to={Pathnames.public.notFound} replace />}
+      />
+    </Routes>
+  );
 };
