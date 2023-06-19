412ae1fa3f9b9daabd2f587d6d2305f1c72c1e87
236677 Trela Oskar <236677@edu.p.lodz.pl>
2023-06-19 22:29:48 +0200

    SSBD202306-398 | Naprawa błędów przy budowaniu frontendu

--- deployment/payara/deploy-payara.sh
+++ deployment/payara/deploy-payara.sh
@@ -4,4 +4,4 @@
 docker pull matino1/ssbd06:payara-1
 
 # Run the Payara container and deploy the WAR file
-docker run -d --name payara-container -p 9191:8181 -v /var/lib/jenkins/workspace/SSBD202306/target/ssbd06.war:/opt/payara/deployments/ssbd06.war matino1/ssbd06:payara-1
+docker run -d --name payara-container -p 9090:8080 -v /var/lib/jenkins/workspace/SSBD202306/target/ssbd06.war:/opt/payara/deployments/ssbd06.war matino1/ssbd06:payara-1
