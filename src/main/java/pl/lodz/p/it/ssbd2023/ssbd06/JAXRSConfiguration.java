9759c90b5c2b126d88c5e30d2d078c7c1d8b2b2b
236677 Trela Oskar <236677@edu.p.lodz.pl>
2023-10-08 14:18:47 +0200

    SSBD202306-123 | Dodanie wstępnej skonteneryzowanej konfiguracji dla aplikacji

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd06/JAXRSConfiguration.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd06/JAXRSConfiguration.java
@@ -4,12 +4,19 @@ import static pl.lodz.p.it.ssbd2023.ssbd06.service.security.Permission.ADMINISTR
 import static pl.lodz.p.it.ssbd2023.ssbd06.service.security.Permission.FACILITY_MANAGER;
 import static pl.lodz.p.it.ssbd2023.ssbd06.service.security.Permission.OWNER;
 
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
 import jakarta.annotation.security.DeclareRoles;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+@QuarkusMain
 @ApplicationPath("/api")
 @DeclareRoles({ADMINISTRATOR, OWNER, FACILITY_MANAGER})
 public class JAXRSConfiguration extends Application {
+
+    public static void main(String ... args) {
+        Quarkus.run(args);
+    }
 
 }
