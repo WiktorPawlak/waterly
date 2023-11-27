a8b5e8bed7fd6edbb20a06fb829114b2229b8c00
236707 Szymon Zakrzewski <sahya230201@gmail.com>
2023-11-27 15:19:26 +0100

    add exit endpoint

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd06/controllers/HealthCheckController.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd06/controllers/HealthCheckController.java
@@ -1,7 +1,10 @@
 package pl.lodz.p.it.ssbd2023.ssbd06.controllers;
 
+import pl.lodz.p.it.ssbd2023.ssbd06.service.security.OnlyGuest;
+
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -15,5 +18,14 @@ public class HealthCheckController {
     @Produces(MediaType.APPLICATION_JSON)
     public Response checkHealth() {
         return Response.ok().build();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/exit")
+    @OnlyGuest
+    public Response exitApplication() {
+        System.exit(999);
+        return Response.ok("Exited successfully").build();
     }
 }
